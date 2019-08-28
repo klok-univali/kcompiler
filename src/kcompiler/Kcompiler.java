8882523ddca58429eeddc5044841c26068cccad7
klok <gabriel@klokenterprise.com.br>
2019-08-28 00:04:41 -0300

    Construção inicial da tela finalizada

---
+++ src/kcompiler/Kcompiler.java
@@ -0,0 +1,18 @@
+/**
+ * @author Gabriel Hegler Klok
+ * @since 2019/08
+ */
+package kcompiler;
+
+import view.KcompilerView;
+
+public class Kcompiler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        KcompilerView.startScreen(args);
+    }
+    
+}
